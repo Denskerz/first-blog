c130f51e16d5c438defb0428070e6e52bf42c457
Denskerz <de.errr18@gmail.com>
2023-12-06 20:46:15 +0300

    hosts have been added

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-36_a!*9j08x2_t)*0&zhlyc08qux%e5xv7#-4*l8zmcy5igs5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
