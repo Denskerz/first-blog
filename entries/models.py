e5dd6da30669b59a872370dd84ccf222c12d0fd4
Denskerz <de.errr18@gmail.com>
2023-12-28 16:25:25 +0300

    Tests has been added

--- entries/models.py
+++ entries/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
         
 class Entry(models.Model): # Создаём новый класс, который будет служить для блога моделью, указывая все необходимые элементы.
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_created = models.DateTimeField(default=timezone.now)
  
+    def get_absolute_url(self):
+        return reverse('entry-detail', kwargs={'pk': self.pk})
+
     def __str__(self): # С помощью функции меняем то, как будет представлена запись в модели.
         return self.title # Указываем, что она будет идентифицироваться с помощью своего заголовка.
  
--- entries/tests.py
+++ entries/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Entry
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.entry = Entry.objects.create(title='Test Post', content='This is a test post.')
+
+    def test_title(self):
+        self.assertEqual(self.entry.title, 'Test Post')
+
+    def test_content(self):
+        self.assertEqual(self.entry.content, 'This is a test post.')
+
+    def test_get_absolute_url(self):
+        expected_url = reverse('entry-detail', kwargs={'pk': self.entry.pk})
+        self.assertEqual(self.entry.get_absolute_url(), expected_url)
+
+
+class PostViewTest(TestCase):
+    def setUp(self):
+        self.entry = Entry.objects.create(title='Test Post', content='This is a test post.')
+
+    def test_post_list_view(self):
+        url = reverse('entry-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'entries/entry_list.html')
+
+    def test_post_detail_view(self):
+        url = reverse('entry-detail', kwargs={'pk': self.entry.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'entries/entry_detail.html')
+        self.assertContains(response, 'Test Post')
+        self.assertContains(response, 'This is a test post.')
+
+    def test_post_create_view(self):
+        url = reverse('entry-create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'entries/entry_form.html')
+
+    def test_post_update_view(self):
+        url = reverse('entry-update', kwargs={'pk': self.entry.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'entries/entry_form.html')
+
+    def test_post_delete_view(self):
+        url = reverse('entry-delete', kwargs={'pk': self.entry.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'entries/entry_confirm_delete.html')
